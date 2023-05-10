8fafe2f8536d188ddda6fbd9d0c0da98be2558cf
Mansi Jain <70169492+Mansijain06@users.noreply.github.com>
2023-05-10 20:34:03 +0530

    Initial commit

---
+++ Array/reverseArrUsingNewArr.js
@@ -0,0 +1,10 @@
+//Reverse Array using New Array
+
+let arr = [1, 2, 3, 4];
+let finalArr = [];
+
+for (let i = 0; i < arr.length; i++) {
+    finalArr[i] = arr[arr.length - 1 - i];
+}
+
+console.log(finalArr)
